a183108d72ce327017b03d185d694a9cc0a0f871
Bill Martin <bill@billmartin.io>
2022-04-03 23:54:41 -0400

    Add examples for exponents and floor division

--- code/09-arithmetic.py
+++ code/09-arithmetic.py
@@ -7,3 +7,9 @@ print(124 % 3)
 
 # 5 ^ 3rd power
 print(5 ** 3)
+
+# Floor division:
+print(9 // 2)     # prints: 4
+print(9.0 // 2.0) # prints: 4.0
+print(-11 // 3)   # prints: -4
+print(-11.0 // 3) # prints: -4.0
