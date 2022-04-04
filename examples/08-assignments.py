806c0dc45304b6d54d7486bbd913566080705838
Bill Martin <bill@billmartin.io>
2022-04-04 17:56:00 -0400

    Add additional code examples for multiple assignments

--- examples/08-assignments.py
+++ examples/08-assignments.py
@@ -1,4 +1,13 @@
-# one-liner multi-assignments
-a, b, c = 3, 4, 5
-
+# Assign multiple variables to same value
+a = b = c = 1
 print(a, b, c)
+
+# Array changing
+a = b = c = ['test']
+b.append('123')
+print(a, b, c)
+
+# Assign multiple variables to multiple values
+a, b, c = 3, 4, 5
+print(a, b, c)
+
